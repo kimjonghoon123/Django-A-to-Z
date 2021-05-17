3c80459f4997f4918e7b9b7e55b37633c855cce5
kjh <aam42@naver.com>
2021-05-17 18:42:12 +0900

    파일올리기,이미지관리

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
-
+import os
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
 
+    head_image = models.ImageField(upload_to='blog/images/%Y/%M/%d/', blank = True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    file_upload = models.FileField(upload_to='blog/images/%Y/%M/%d/', blank = True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
 
     def get_absolute_url(self):
-        return f'/blog/{self.pk}'
+        return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_ext(self):
+        return self.file_upload.name().split('.')[-1]
+
