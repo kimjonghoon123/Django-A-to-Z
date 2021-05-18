57e97f98cb0dfe2f12fd5ee7deefa51a6fb0645c
kjh <aam42@naver.com>
2021-05-18 10:36:14 +0900

    5월17일 공부

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,16 @@ from django.db import models
 
 import os
 class Post(models.Model):
+    head_image = models.ImageField(upload_to='blog/images/%Y/%M/%d/', blank=True)
     title = models.CharField(max_length=30)
     content = models.TextField()
-
+    hook_text = models.CharField(max_length=100, blank= True)
     head_image = models.ImageField(upload_to='blog/images/%Y/%M/%d/', blank = True)
+    file_upload = models.FileField(upload_to='blog/images/%Y/%M/%d/', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    file_upload = models.FileField(upload_to='blog/images/%Y/%M/%d/', blank = True)
+    hook_text = models.CharField(max_length=100, blank=True)
+
 
     def __str__(self):
         return f'[{self.pk}]{self.title}'
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from bs4 import BeautifulSoup
+from .models import Post
 
-# Create your tests here.
+class TestView(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_post_list(self):
+        #1.1. 포스트 목록 페이지 가져옴
+        response = self.client.get('/blog')
+        # 1.2 정상적으로 페이지가 로드 됨
+        self.assertEqual(response.status_code, 200)
+        # 1.3 페이지 타이틀은 'blog'이다
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.arrsertEqual(soup.title.text, 'Blog')
+        # 1.4 내이게이션 바가 있다.
+        navbar = soup.nav
+        # 1.5 blog, about me라는 내비게이션 바에 있다.
+        self.assertIn('Blog', navbar.text)
+        self.asertIn('About Me', navbar.text)
+        # 2.1포스트가 하나도 없다면
+        self.assertEqual(Post.objects.count(), 0)
+        # 2.2main area에 아직 게시물이 없습니다. 라는 문구가 나타난다
